a10efb442fa8b81ce7da23d0caf81a1188bd7242
FrancoDelport <frnco_16@hotmail.com>
2020-12-21 23:46:30 -0300

    Conexion a la base de datos

---
+++ database.php
@@ -0,0 +1,7 @@
+<?php
+$HOST="localhost";
+$USERNAME="adm_webgenerator";
+$PASSWORD="webgenerator2020";
+$NOMBREDB="webgenerator";
+$conexion = mysqli_connect("$HOST, $USERNAME, $PASSWORD, $NOMBREDB"); 
+?>
